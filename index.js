4ccb323cb9306de6ed28c6e253b5ec58d288a531
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-04-24 14:49:47 -0500

    Morgan configuration and setup completet
    Logs request headers as well as body for POST requests. Exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-morgan("tiny");
-
-app.use(express.json());
+var logger = morgan("tiny");
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
+  console.log("Path :", request.path);
+  console.log("Body :", request.body);
   console.log("---");
   next();
 };
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
+
+morgan.token("body", function getBody(req) {
+  return JSON.stringify(req.body);
+});
+
+app.use(express.json());
+// app.use(logger);
+app.use(
+  morgan(":method :url :status :res[content-length] :response-time :body")
+);
 
 let persons = [
   {
@@ -102,6 +109,8 @@ app.delete("/api/persons/:id", (request, response) => {
 
   response.status(204).end();
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
