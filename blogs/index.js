20669f480d9f80caacd30759043b2e3782cca10d
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-06-05 17:03:10 -0500

    It always worked I am a fool
    some reason thought the paths changed to remove the /api/blogs pathing whooooops

--- blogs/index.js
+++ blogs/index.js
@@ -1,72 +1,7 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const Blog = require("../blogs/models/blog");
+const app = require("./app");
 const config = require("./utils/config");
-const url = config.MONGODB_URI;
-// const app = require("./app");
 const logger = require("./utils/logger");
-const middleware = require("./utils/middleware");
-
-mongoose.connect(url);
-
-app.use(cors());
-
-app.use(express.json());
-app.use(middleware.requestLogger);
-// app.listen(config.PORT, () => {
-//   logger.info(`Server running on port ${config.PORT}`);
-// });
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.get("/api/blogs/:id", (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then((blog) => {
-      if (blog) {
-        response.json(blog);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-// const unknownEndpoint = (request, response) => {
-//   response.status(404).send({ error: "unknown endpoint" });
-// };
-
-// app.use(unknownEndpoint);
-app.use(middleware.unknownEndpoint);
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+  logger.info(`Server running on port ${config.PORT}`);
 });
-
-// const errorHandler = (error, request, response, next) => {
-//   console.error(error.message);
-
-//   if (error.name === "CastError") {
-//     return response.status(400).send({ error: "malformatted id" });
-//   } else if (error.name === "ValidationError") {
-//     return response.status(400).json({ error: error.message });
-//   }
-//   next(error);
-// };
-
-// app.use(errorHandler);
-app.use(middleware.unknownEndpoint);
